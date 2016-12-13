a30e4e79624df0b6fb480427ffc017c13892a67d
Brenton Sutherland <brenton311@users.noreply.github.com>
2016-12-13 03:10:58 -0500

    Create stream_video.sh

---
+++ stream_video.sh
@@ -0,0 +1,1 @@
+gst-launch-1.0 rpicamsrc rotation=180 bitrate=2000000  !  'video/x-h264,width=640,height=480'  ! h264parse     ! queue     ! rtph264pay config-interval=1 pt=96     ! gdppay     ! udpsink host=192.168.43.1 port=9000
